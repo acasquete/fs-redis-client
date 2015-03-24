9f95a6573b21dabc33f32d8daee25dea33d044e2
Alex Casquete <alex@casquete.es>
2015-03-25 00:09:43 +0100

    Fix regular expression

--- Redis.Client/Script.fsx
+++ Redis.Client/Script.fsx
@@ -1,10 +1,11 @@
 ﻿open System.Text.RegularExpressions
 
 //let s = "eval \"return {KEYS[1],KEYS[2],ARGV[1],ARGV[2]}\" 2 key1 key2 first second"
-let s = "set my:key.{}... \"12345 6788\""
+//let s = "set my:key.{}... \"12345 6788\""
+let s = "lpush lista:d \"akslask\" 1 djkj dkjsdk \"jdksdj\" jkfjdk \"jdksjdksdj kjdskdj\""
 
 let parseCommand (line:string) = 
-    Regex.Matches(line.Trim(), @"(?<m>[\w.:{}]+)|\""(?<m>[\s\S]*)""")
+    Regex.Matches(line.Trim(), @"(?<m>[\w,.:\[\]{}]+)|\""(?<m>[\s\w]*)""")
     |> Seq.cast<Match>
     |> Seq.map (fun x -> x.Groups.["m"].Value)
     |> Seq.toArray
